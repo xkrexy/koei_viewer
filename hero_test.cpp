dd910abe3073b31542b4d83b0cb30fe8a307703e
Gyeongcheol Jang <gch.jang@samsung.com>
2021-12-06 14:12:43 +0900

    Parsing header

--- hero_test.cpp
+++ hero_test.cpp
@@ -4,51 +4,53 @@
 
 int main(int argc, char *argv[]) {
     FILE *fp = fopen("hero/FACEDAT.R3", "r");
+    //FILE *fp = fopen("sam4/KAODATA.S4", "r");
     fseek(fp, 0, SEEK_END);
     size_t filesize = ftell(fp);
     fseek(fp, 0, SEEK_SET);
 
     uint8_t *buf = new uint8_t[filesize];
     size_t ret = fread(buf, 1, filesize, fp);
-    printf("FileSize: %d / Read: %d\n", filesize, ret);
+    printf("FileSize: %ld / Read: %ld\n", filesize, ret);
     fclose(fp);
 
     buf_reader_t *reader = create_buffer_reader(buf, filesize, false);
-    int header = read_int32(reader);
-    // header must be 0x00000000
 
     int index = 0;
-    while (index < 240) {
-        int curpos = buf_get_seek_pos(reader);
-        int datasize = read_uint16(reader);
+    while (!buf_end(reader)) {
         int address = read_uint32(reader);
-        printf("Value[%d]: %5d %8d\n", index, datasize, address);
+        int datasize = read_uint16(reader);
+        int curpos = buf_get_seek_pos(reader);
+        printf("Value[%3d]: Seek(%5d) %8d %5d\n", index, curpos - 6, address, datasize);
 
-        char path[32];
-        sprintf(path, "temp/%03d.dat", index);
-        FILE *fp = fopen(path, "w");
+        // Save uncompressed file (testing)
+        // char path[32];
+        // sprintf(path, "temp/%03d.dat", index);
+        // FILE *fp = fopen(path, "w");
 
-        uint8_t *comp = new uint8_t[datasize];
-        uint8_t *uncomp = new uint8_t[1920];
+        // uint8_t *comp = new uint8_t[datasize];
+        // uint8_t *uncomp = new uint8_t[1920];
 
-        memset(comp, 0, datasize);
-        memset(uncomp, 0, 1920);
+        // memset(comp, 0, datasize);
+        // memset(uncomp, 0, 1920);
 
-        buf_seek(reader, address);
-        read_bytes(reader, comp, datasize);
+        // buf_seek(reader, address);
+        // read_bytes(reader, comp, datasize);
 
-        fat_decode(comp, datasize, uncomp, 1920);
-        fwrite(uncomp, 1, 1920, fp);
+        // fat_decode(comp, datasize, uncomp, 1920);
+        // fwrite(uncomp, 1, 1920, fp);
 
-        fclose(fp);
+        // fclose(fp);
 
-        delete[] comp;
-        delete[] uncomp;
+        // delete[] comp;
+        // delete[] uncomp;
 
         buf_seek(reader, curpos);
 
         index++;
     }
+
+    printf("Last buffer seek pos: %d(0x%08x)\n", buf_get_seek_pos(reader), buf_get_seek_pos(reader));
 
     delete[] buf;
 
