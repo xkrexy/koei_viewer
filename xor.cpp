07503756bf6229efdc06c7af4ca3892dce0932a5
Gyeongcheol Jang <gcjjyy@icloud.com>
2021-12-12 23:42:14 +0900

    Testing..

--- xor.cpp
+++ xor.cpp
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)       \
-    (byte & 0x80 ? '1' : '0'),     \
-        (byte & 0x40 ? '1' : '0'), \
-        (byte & 0x20 ? '1' : '0'), \
-        (byte & 0x10 ? '1' : '0'), \
-        (byte & 0x08 ? '1' : '0'), \
-        (byte & 0x04 ? '1' : '0'), \
-        (byte & 0x02 ? '1' : '0'), \
-        (byte & 0x01 ? '1' : '0')
-
 int one_count(uint8_t value)
 {
     int count = 0;
@@ -26,65 +15,106 @@ int one_count(uint8_t value)
     return count;
 }
 
-void output(uint8_t byte)
+char *shortString(uint16_t word)
 {
-    uint8_t upper_half = (byte & 0xf0) >> 4;
+    static char _str[17];
+    for (int i = 0; i < 16; i++)
+    {
+        _str[15 - i] = '0' + ((word >> i) & 0x01);
+    }
 
-    printf("0x%02X: " BYTE_TO_BINARY_PATTERN " : 0x%02X (" BYTE_TO_BINARY_PATTERN ")\n",
-           byte,
-           BYTE_TO_BINARY(byte),
-           upper_half,
-           BYTE_TO_BINARY(upper_half));
+    _str[16] = 0;
+    return _str;
+}
+
+char *bitString(uint8_t byte)
+{
+    static char _str[9];
+    for (int i = 0; i < 8; i++)
+    {
+        _str[7 - i] = '0' + ((byte >> i) & 0x01);
+    }
+
+    _str[8] = 0;
+    return _str;
+}
+
+char *nibbleString(uint8_t byte)
+{
+    static char _str[5];
+    for (int i = 0; i < 4; i++)
+    {
+        _str[3 - i] = '0' + ((byte >> i) & 0x01);
+    }
+
+    _str[4] = 0;
+    return _str;
+}
+
+void Testing(uint32_t offset, uint8_t *data, size_t size)
+{
+    int sum = 0;
+    int upper_half_sum = 0;
+    int lower_half_sum = 0;
+
+    printf("[0x%04X %s]\n", offset, shortString(offset));
+    for (size_t i = 0; i < size; i++)
+    {
+        uint8_t upper_half = (data[i] & 0xf0) >> 4;
+        uint8_t lower_half = (data[i] & 0x0f) >> 0;
+
+        printf("0x%02X: %s\n",
+               data[i],
+               bitString(data[i]));
+
+        if (i >= 1 && i < size - 1)
+        {
+            sum ^= upper_half;
+            sum ^= lower_half;
+            upper_half_sum += upper_half;
+            lower_half_sum += lower_half;
+        }
+    }
+
+    printf("[Target Value: 0x%01X %s]\n", data[size - 1] & 0x0f, nibbleString(data[size - 1] & 0x0f));
+
+    printf("Sum: 0x%04X (%d) %s\n", sum, sum, bitString(sum & 0xff));
+    printf("Upper Half Sum: 0x%04X (%d) %s\n", upper_half_sum, upper_half_sum, bitString(upper_half_sum & 0xff));
+    printf("Lower Half Sum: 0x%04X (%d) %s\n", lower_half_sum, lower_half_sum, bitString(lower_half_sum & 0xff));
+
+    printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
     {
         uint8_t data[] = {0x61, 0xA9, 0x55, 0x74};
-        for (int i = 0; i < sizeof(data); i++)
-        {
-            output(data[i]);
-        }
+        Testing(0x29, data, sizeof(data));
     }
-
-    printf("========\n");
 
     {
         uint8_t data[] = {0x61, 0xD7, 0xCB, 0x7F};
-        for (int i = 0; i < sizeof(data); i++)
-        {
-            output(data[i]);
-        }
+        Testing(0x162, data, sizeof(data));
     }
 
-    printf("========\n");
+    {
+        uint8_t data[] = {0x61, 0xDF, 0xD5, 0x7F};
+        Testing(0x27a, data, sizeof(data));
+    }
 
     {
         uint8_t data[] = {0x61, 0x5F, 0x9F, 0x76};
-        for (int i = 0; i < sizeof(data); i++)
-        {
-            output(data[i]);
-        }
+        Testing(0x40, data, sizeof(data));
     }
 
-    printf("========\n");
-
     {
-        uint8_t data[] = {0x62, 0x54, 0xAA, 0x29, 0x0C, 0x73};
-        for (int i = 0; i < sizeof(data); i++)
-        {
-            output(data[i]);
-        }
+        uint8_t data[] = {0xA4, 0xFF, 0x76};
+        Testing(0x4B, data, sizeof(data));
     }
 
-    printf("========\n");
-
     {
-        uint8_t data[] = {0xA1, 0xEE, 0xA4, 0xDF, 0xA2, 0xEF, 0xA1, 0xF7, 0x7E};
-        for (int i = 0; i < sizeof(data); i++)
-        {
-            output(data[i]);
-        }
+        uint8_t data[] = {0xA1, 0xFB, 0xA0, 0xF7, 0x70};
+        Testing(0x55, data, sizeof(data));
     }
 
     return 0;
